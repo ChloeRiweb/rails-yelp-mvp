769fe17c8822253f0bd6fc4b06f962fed2cf9b88
chloerihouay <chloerihouay@hotmail.com>
2020-08-12 14:42:37 +0200

    updating yelp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,18 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+end
