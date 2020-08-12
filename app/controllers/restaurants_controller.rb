f522debe85b1ec6152d4e9063f7fe71bd9c33a8c
chloerihouay <chloerihouay@hotmail.com>
2020-08-12 15:53:27 +0200

    yelp finished ! missing the magic mise en forme

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,10 +9,21 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
